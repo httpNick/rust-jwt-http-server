e3aca12ae13622b7bbda217cd496f5f7f9ddb3b3
httpNick <nick.b.duncan@gmail.com>
2025-06-06 20:52:22 -0700

    Added a README, added .env to gitignore and implemented start of HTTP server that has one route which is /login. This route generates a JWT and returns it as a successful response as long as the user's credentials are valid.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,48 @@
+use actix_web::{App, HttpResponse, HttpServer, Responder, post, web};
+use dotenv::dotenv;
+use jwt::generate_jwt;
+use serde::{Deserialize, Serialize};
+use std::env;
+
 mod jwt;
 
-fn main() {
-    println!("Hello, world!");
+#[derive(Debug, Serialize, Deserialize)]
+struct AuthRequest {
+    username: String,
+    password: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct AuthResponse {
+    token: String,
+}
+
+#[post("/auth/login")]
+async fn login(req_body: web::Json<AuthRequest>) -> impl Responder {
+    dotenv().ok();
+    let jwt_secret = env::var("JWT_SECRET").expect("JWT_SECRET must be set in env.");
+    // some basic auth, username/password must match what is in .env file.
+    let username = env::var("USER_NAME").expect("USER_NAME must be set in env.");
+    let password = env::var("USER_PASSWORD").expect("USER_PASSWORD must be set in env.");
+    if username == req_body.username && password == req_body.password {
+        let permissions = vec![String::from("read")];
+        let token = generate_jwt(&username, permissions, jwt_secret.as_bytes())
+            .expect("Failed to generate JWT.");
+        return HttpResponse::Ok().json(AuthResponse { token });
+    }
+    HttpResponse::Unauthorized().body("Invalid credentials.")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    dotenv().ok();
+    let port = env::var("PORT").unwrap_or_else(|_| String::from("8080"));
+    let bind_address = format!("127.0.0.1:{}", port);
+
+    println!("Starting server at {}", bind_address);
+
+    HttpServer::new(|| App::new().service(login))
+        .bind(&bind_address)?
+        .run()
+        .await
 }
